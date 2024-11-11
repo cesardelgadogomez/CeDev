f8a7580267e4f8978235a45b0aa71541bf14e096
cesardelgadogomez <cesadelgado2110@gmail.com>
2024-11-11 16:44:22 -0500

    sección quién soy casi finalizada. Empezada la sección habilidades.

--- js/main.js
+++ js/main.js
@@ -9,3 +9,27 @@ abrirMenu.addEventListener("click", () => {
 cerrarMenu.addEventListener("click", () => {
   nav.classList.remove("nav-visible");
 })
+
+//Habilidades
+
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Función para activar animaciones cuando el elemento es visible
+function activateAnimations() {
+  const skillsContainer = document.getElementById('skills');
+  if (isInViewport(skillsContainer)) {
+    skillsContainer.classList.add('animate');
+    skillsContainer.style.opacity = 1; // Mostrar la sección al ser visible en el scroll
+    window.removeEventListener('scroll', activateAnimations); // Remover evento después de activación
+  }
+}
+
+window.addEventListener('scroll', activateAnimations);
