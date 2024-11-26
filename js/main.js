9f18a1d853eeda2ea67b3ecd8eb55f4220a2175f
cesardelgadogomez <cesadelgado2110@gmail.com>
2024-11-26 18:26:04 -0500

    Se arreglan unos errores en el código de las imágenes

--- js/main.js
+++ js/main.js
@@ -13,7 +13,6 @@ cerrarMenu.addEventListener("click", () => {
 })
 
 
-
 //Typewritter
 
 const text = "NUNCA ES TARDE PARA EMPEZAR. LO QUE IMPORTA ES QUE COMIENCES.";
@@ -31,55 +30,7 @@ function typeEffect() {
 typeEffect();
 
 
-//Habilidades
-
-/* function isInViewport(element) {
-  const rect = element.getBoundingClientRect();
-  return (
-    rect.top >= 0 &&
-    rect.left >= 0 &&
-    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-  );
-}
-
-// Función para activar animaciones cuando el elemento es visible
-function activateAnimations() {
-  const skillsContainer = document.getElementById('skills');
-  if (isInViewport(skillsContainer)) {
-    skillsContainer.style.opacity = 1; // Mostrar la sección al ser visible en el scroll
-    const bars = document.querySelectorAll('.bar-fill');
-    bars.forEach(bar => {
-      const animation = bar.getAttribute('data-animation');
-      bar.style.animation = animation; // Activar animación para cada barra
-    });
-    window.removeEventListener('scroll', activateAnimations); // Remover evento después de activación
-  }
-}
-
-// Agregar evento de scroll
-window.addEventListener('scroll', activateAnimations);
-
-
-//Animación de tecnologías
-
-function activateCircles() {
-  const techContainer = document.getElementById('technologies');
-  if (isInViewport(techContainer)) {
-    techContainer.style.opacity = 1;
-    const circles = document.querySelectorAll('.circle');
-    circles.forEach(circle => {
-      const percentage = circle.getAttribute('data-percentage');
-      const progress = circle.querySelector('.progress');
-      const offset = 314 - (314 * percentage) / 100;
-      progress.style.strokeDashoffset = offset; // Aquí se anima cada círculo
-    });
-    window.removeEventListener('scroll', activateCircles);
-  }
-}
-
-window.addEventListener('scroll', activateCircles); */
-
+//Animación de barras
 function isInViewport(element) {
   const rect = element.getBoundingClientRect();
   return (
@@ -132,3 +83,4 @@ document.addEventListener('DOMContentLoaded', activateAnimations);
 window.addEventListener('scroll', activateCircles);
 window.addEventListener('resize', activateCircles);
 document.addEventListener('DOMContentLoaded', activateCircles);
+
